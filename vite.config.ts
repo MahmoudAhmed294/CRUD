a0b8458ced85dd1f476dd9b865c4482a6dedc0e8
MahmoudAhmed294 <60276598+MahmoudAhmed294@users.noreply.github.com>
2022-11-16 18:24:06 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {    // <-- this object is added
-    port: 8000
+    port: 8080
   },
   plugins: [react()],
   
